d3cfb5ec73de668aff84b12dc2518da1d35245db
Ebenezer <Ebenezer@Younow.com>
2019-03-06 23:27:04 -0500

    delete some unused classes

--- app/src/main/java/jfyg/buttonfun/ErrorStrings.kt
+++
@@ -1,3 +0,0 @@
-package jfyg.buttonfun
-
-const val DIMENS_NOT_CALCULATED = "Device dimensions has not been calculated yet, did you call setScreenDimens() from SquareGenerator?"
---
+++ utils/src/main/java/jfyg/utils/ErrorStrings.kt
@@ -0,0 +1,4 @@
+package jfyg.utils
+
+const val DIMENS_NOT_CALCULATED_ERROR = "Device dimensions has not been calculated yet, did you call setScreenDimens() from SquareGenerator?"
+const val DEVICE_ORIENTATION_UNEXPECTED_ERROR = "Device orientation not returned as 0*,90*,180* or 270*"
